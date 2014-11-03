8c10f7b412e8a063c8ce5afbed235c81a8023a73
Maxim Shchetynin <git@zeppelinium.de>
2014-11-03 10:56:12 +0100

    Initial Commit

---
+++ GKrellM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GKrellM
+//
+//  Created by Maxim Shchetynin on 03.11.14.
+//  Copyright (c) 2014 Meine Werke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GKrellM/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GKrellM
+//
+//  Created by Maxim Shchetynin on 03.11.14.
+//  Copyright (c) 2014 Meine Werke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
